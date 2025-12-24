ffbbdc89624787cd0e44d5995691b5f0ed6a5d53
Lucia Smith <lucia@luciaesmith.com>
2025-12-24 04:07:06 -0800

    Do not list web games if there is no internet connection.

--- scanners/web.py
+++ scanners/web.py
@@ -6,5 +6,7 @@ def generate_web_pages():
     manual_config_path = "./config/web.json"
     games = read_json(manual_config_path)["games"]
     for game in games:
-        output_json[game] = {"script": "", "web": games[game]}
+        # Let's just ping google here
+        check_domain = "google.com"
+        output_json[game] = {"script": "", "web": games[game], "ip": check_domain, "live_check": check_domain}
     return output_json
