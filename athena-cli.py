089ff6f6fdc120452a304ae87807c7dd7a2dd533
Lucia Smith <lucia@luciaesmith.com>
2026-01-09 01:52:35 -0800

    Remove development logging from CLI.

--- athena-cli.py
+++ athena-cli.py
@@ -1,9 +1,9 @@
 import sys
 
+from config_lib.install import InstallConfig, create_initial_configs
 from launcher.exec import setup_and_launch
 from launcher.lib.config import read_json
 from launcher.time_keep import validate_whitelisted_days
-from config_lib.install import create_initial_configs, InstallConfig
 
 
 def flatten_config(config):
@@ -23,9 +23,7 @@ def process_config_data():
 
 
 def find_program_args(all_programs, program_to_call):
-    print(all_programs)
     for program in all_programs:
-        print(program["name"])
         if program["name"] == program_to_call:
             return program
     return None
@@ -33,10 +31,8 @@ def find_program_args(all_programs, program_to_call):
 
 def process_user_input():
     all_programs = process_config_data()
-    print(all_programs)
     program_to_call = sys.argv[1]
     program_args = find_program_args(all_programs, program_to_call)
-    print(program_args)
 
     is_logging_time = False
     if program_args is None:
